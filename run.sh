c839d2d24a876a57b9670aca1c0d90018b0eebad
Daichi Hirobe <hirobe.daichi@lab.ime.cmc.osaka-u.ac.jp>
2024-02-12 17:49:36 +0900

    最小限のカーネルがビルドされる。
    ただし、メモリマップを出力することはできない

--- run.sh
+++ run.sh
@@ -5,4 +5,12 @@ set -xue
 # QEMUのファイルパス
 QEMU=qemu-system-riscv32
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+CC=clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# カーネルのビルドコマンド
+$CC $CFLAGS -Wl, -Tkernel.ld -o kernel.elf kernel.c
+
+# QEMUの実行
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
