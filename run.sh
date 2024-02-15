ef2a0ef693f8ac880d4f93c8277b8ea6ff918043
Daichi Hirobe <hirobe.daichi@lab.ime.cmc.osaka-u.ac.jp>
2024-02-15 15:37:20 +0900

    ユーザーランドのプログラムを作成

--- run.sh
+++ run.sh
@@ -9,8 +9,15 @@ CC=clang
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+OBJCOPY=llvm-objcopy
+
+# シェルをビルド
+$CC $CFLAGS -Wl,-Tuser.ld -Wl.-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # カーネルのビルドコマンド
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c test.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c test.c shell.bin.o
 
 # QEMUの実行
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
