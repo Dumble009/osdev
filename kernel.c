40f10589b2fae54d0d413080d50b296361fd0bff
Daichi Hirobe <hirobe.daichi@lab.ime.cmc.osaka-u.ac.jp>
2024-02-18 18:27:34 +0900

    ファイルの読み書き機能を作成

--- kernel.c
+++ kernel.c
@@ -189,6 +189,114 @@ void read_write_disk(void *buf, unsigned sector, int is_write)
     printf("read_write_disk: finish the function\n");
 }
 
+struct file files[FILES_MAX];
+uint8_t disk[DISK_MAX_SIZE];
+
+/// @brief 8進数から10進数への変換
+/// @param oct 8進数値を持つ文字列
+/// @param len octの長さ。ヌル文字の文は考慮しない
+/// @return octを10進数に変換した数値
+int oct2int(char *oct, int len)
+{
+    int dec = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (oct[i] < '0' || oct[i] > '7')
+        {
+            break;
+        }
+
+        dec = dec * 8 + (oct[i] - '0');
+    }
+
+    return dec;
+}
+
+/// @brief ファイルシステムの初期化
+void fs_init(void)
+{
+    // ディスクイメージの中身を全て読み込む
+    for (unsigned sector = 0; sector < sizeof(disk) / SECTOR_SIZE; sector++)
+    {
+        read_write_disk(&disk[sector * SECTOR_SIZE], sector, false);
+    }
+
+    // メタデータとデータ本体を取り出して再構成
+    unsigned off = 0;
+    for (int i = 0; i < FILES_MAX; i++)
+    {
+        struct tar_header *header = (struct tar_header *)&disk[off];
+        if (header->name[0] == '\0')
+        {
+            break;
+        }
+
+        if (strcmp(header->magic, "ustar") != 0)
+        {
+            PANIC("invalid tar header: magic=\"%s\"", header->magic);
+        }
+
+        int filesz = oct2int(header->size, sizeof(header->size));
+        struct file *file = &files[i];
+        file->in_use = true;
+        strcpy(file->name, header->name);
+        memcpy(file->data, header->data, filesz);
+        file->size = filesz;
+        printf("file: %s, size=%d\n", file->name, file->size);
+
+        off += (uint32_t)align_up(sizeof(struct tar_header) + filesz, SECTOR_SIZE);
+    }
+}
+
+/// @brief ディスクへのデータの書き戻し
+void fs_flush(void)
+{
+    // files変数の各ファイルの内容をdisk変数に書き込み、
+    // それをディスクイメージに書き込む
+    memset(disk, 0, sizeof(disk));
+    unsigned off = 0;
+    for (int file_i = 0; file_i < FILES_MAX; file_i++)
+    {
+        struct file *file = &files[file_i];
+        if (!file->in_use)
+        {
+            continue;
+        }
+
+        struct tar_header *header = (struct tar_header *)&disk[off];
+        memset(header, 0, sizeof(*header));
+        strcpy(header->name, file->name);
+        strcpy(header->mode, "000644");
+        strcpy(header->magic, "ustr");
+        strcpy(header->version, "00");
+        header->type = '0';
+
+        // ファイルサイズを8進数文字列に変換
+        int filesz = file->size;
+        for (int i = sizeof(header->size); i > 0; i--)
+        {
+            header->size[i - 1] = (filesz % 8) + '0';
+            filesz /= 8;
+        }
+
+        // チェックサムの計算
+        int checksum = ' ' * sizeof(header->checksum);
+        for (unsigned i = 0; i < sizeof(struct tar_header); i++)
+        {
+            checksum += (unsigned char)disk[off + i];
+        }
+        for (int i = 5; i >= 0; i--)
+        {
+            header->checksum[i] = (checksum % 8) + '0';
+            checksum /= 8;
+        }
+
+        // ファイルデータをコピー
+        memcpy(header->data, file->data, file->size);
+        off += (uint32_t)align_up(sizeof(struct tar_header) + file->size, SECTOR_SIZE);
+    }
+}
+
 extern char __bss[], __bss_end[], __stack_top[];
 
 /// @brief SBIの処理を呼び出す関数
@@ -610,13 +718,7 @@ void kernel_main(void)
     // 例外ハンドラのアドレスをレジスタに登録
     WRITE_CSR(stvec, (uint32_t)kernel_entry);
     virtio_blk_init();
-
-    char buf[SECTOR_SIZE];
-    read_write_disk(buf, 0, false);
-    printf("first sector: %s\n", buf);
-
-    strcpy(buf, "hello from kernel!!!\n");
-    read_write_disk(buf, 0, true);
+    fs_init();
 
     // 標準ライブラリのテスト
     run_test();
